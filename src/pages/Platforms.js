3aae6550ce3d6703c9dbdb6a12ba46e4612dc12c
Farah Nagy <farahnagy529@gmail>
2023-09-15 02:36:00 +0300

    platforms

--- src/pages/Platforms.js
+++ src/pages/Platforms.js
@@ -20,7 +20,7 @@ const Platforms = () => {
           to="https://www.facebook.com/KultCreativeProductions"
           target="_blank"
           className="readMoreLink"
-          style={{backgroundColor:"red"}}
+          style={{ backgroundColor: "red" }}
         >
           <button className="readMore">Read More</button>
         </Link>
@@ -30,29 +30,52 @@ const Platforms = () => {
           <img className="crowdBoB" src="./crowdBoB.png" alt="crowdd"></img>
         </div>
       </div>
-      {/* <div className="battleOfTheBurgers">
-        <div className="burgerNText">
-          <div className="bobLogo" />
-          <div className="BoBParDiv">
-            <p className="BoBPar">
-              Battle of the Bands is the iconic platform that has become famous
-              for discovering undiscovered talent, giving underground bands the
-              opportunity of a lifetime. This competition gives bands a stage to
-              battle it out, while providing them with the means and guidance to
-              grow, competing to win the title of TOP BAND.
-            </p>
+      <div className="battleOfTheBurgers">
+        <div className="burgerNTongue">
+          <div className="tongue" />
+          <div className="burger" />
+        </div>
+        <div className="logoBurgerTextReadMore">
+          <div className="burgerLogo" />
+          <div className="burgerText">
+            The first battle that started it all. This one-of-a-kind experience
+            gave the burger masters a platform to show off their skills,
+            competing to win over the audience’s taste buds. With a food tasting
+            competition, shopping bazaar, and endless activities to keep the
+            audience engaged, it was the festival the food loving crowd was
+            missing.
           </div>
-          <Link to="https://www.facebook.com/KultCreativeProductions"  target="_blank">
-          <button className="readMore">
-            Read More
-          </button>
+          <Link
+            to="https://www.facebook.com/KultCreativeProductions"
+            target="_blank"
+          >
+            <button className="readMoreBurger">Read More</button>
           </Link>
         </div>
-        <div className="crowdNMic">
-          <div className="MicLogo" />
-         
+      </div>
+      <div className="battleOfTheWheels">
+        <div className="BoWLogoNText">
+          <div className="BoWLogo"/>
+          <div className="BoWText">
+            The first battle that started it all. This one-of-a-kind experience
+            gave the burger masters a platform to show off their skills,
+            competing to win over the audience’s taste buds. With a food tasting
+            competition, shopping bazaar, and endless activities to keep the
+            audience engaged, it was the festival the food loving crowd was
+            missing.
+          </div>
+          <Link
+            to="https://www.facebook.com/KultCreativeProductions"
+            target="_blank"
+          >
+            <button className="readMoreWheels">Read More</button>
+          </Link>
         </div>
-      </div> */}
+        <div className="flagNCar">
+          <div className="flag" />
+          <div className="car" />
+        </div>
+      </div>
     </div>
   );
 };
