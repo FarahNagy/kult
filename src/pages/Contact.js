8025e10493638143ca980b5a5966f59d4fcaf1b1
Farah Nagy <farahnagy529@gmail>
2023-09-16 04:21:01 +0300

    started Contact Form!

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,35 +3,36 @@ import emailjs from "@emailjs/browser";
 import "./contact.css";
 import FooterWithoutClients from "./FooterWithoutClients";
 
-
 const Contact = () => {
   const form = useRef();
-  const [textareaValue, setTextareaValue] = useState('');
-  const [textareaValue2, setTextareaValue2] = useState('');
-  const [textareaValue3, setTextareaValue3] = useState('');
-  const [textareaValue4, setTextareaValue4] = useState('');
-  const [textareaValue5, setTextareaValue5] = useState('');
+  const [textareaValue, setTextareaValue] = useState("");
+  const [textareaValue2, setTextareaValue2] = useState("");
+  const [textareaValue3, setTextareaValue3] = useState("");
+  const [textareaValue4, setTextareaValue4] = useState("");
+  const [textareaValue5, setTextareaValue5] = useState("");
   const sendEmail = (e) => {
-   
-      
+    e.preventDefault();
 
-      e.preventDefault();
-  
-      if (textareaValue.trim() === '' ||textareaValue2.trim() === '' ||textareaValue3.trim() === '' ||textareaValue4.trim() === '' ||textareaValue5.trim() === ''  ) {
-        alert('Textarea must not be empty.');
-        return;
-      }
-  
-      // Perform other form submission logic here
-      alert('Form submitted successfully.');
+    if (
+      textareaValue.trim() === "" ||
+      textareaValue2.trim() === "" ||
+      textareaValue3.trim() === "" ||
+      textareaValue4.trim() === "" ||
+      textareaValue5.trim() === ""
+    ) {
+      alert("Textarea must not be empty.");
+      return;
+    }
 
-      setTextareaValue('');
-      setTextareaValue2('');
-      setTextareaValue3('');
-      setTextareaValue4('');
-      setTextareaValue5('');
+    // Perform other form submission logic here
+    alert("Form submitted successfully.");
 
-    
+    setTextareaValue("");
+    setTextareaValue2("");
+    setTextareaValue3("");
+    setTextareaValue4("");
+    setTextareaValue5("");
+
     e.preventDefault();
 
     emailjs
@@ -51,74 +52,20 @@ const Contact = () => {
       );
   };
 
-  
-
   return (
     <div className="mainContact">
-      <div
-        style={{
-          width: "1100px",
-          height: "600px",
-          display: "flex",
-          flexDirection: "row",
-
-        }}
-        className="contactform"
-      >
-        <div
-          style={{ width: "470px", height: "600px", backgroundColor: "white" , borderTopLeftRadius:'22px',
-          borderBottomLeftRadius:'22px', 
-        }}
-        >
-          <p style={{ marginLeft: "40px", marginTop: "50px", lineHeight: "1" }}>
-            <span style={{ fontSize: "40px", fontFamily: "boldFont", color:'#4aa9ea'}}>
-              YOUR DESIGN,
-            </span>
+      <div className="contactform">
+        <div className="leftWhiteDiv">
+          <p className="yourDesignVoice"> 
+            <span className="design">YOUR DESIGN,</span>
             <br />
-            <span
-              style={{
-                fontSize: "40px",
-                fontFamily: "regFont",
-                marginLeft: "1px",
-                color:'#4aa9ea'
-              }}
-            >
-              YOUR VOICE
-            </span>
+            <span className="voice">YOUR VOICE</span>
           </p>
         </div>
-        <div
-          style={{
-            width: "630px",
-            height: "600px",
-            backgroundColor: "#4aa9ea",
-            display: "flex",
-            flexDirection: "column",
-            borderTopRightRadius:'22px',
-            borderBottomRightRadius:'22px'
-          }}
-        >
-          <h1 style={{ fontFamily: "regFont", marginLeft: "60px", color:'white'}}>
-            {" "}
-            LET'S TALK ABOUT YOUR VISION!
-          </h1>
-          <form
-            ref={form}
-            onSubmit={sendEmail}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifySelf: "flex-start",
-              marginTop: "20px",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                marginLeft: "65px",
-              }}
-            >
+        <div className="rightBlueDiv">
+        <span className="talkAbtVision">LET'S TALK ABOUT YOUR VISION!</span>
+          <form ref={form} onSubmit={sendEmail} className="theForm">
+             <div className="firstLastName">
               <input
                 className="placeholder"
                 type="text"
@@ -126,7 +73,6 @@ const Contact = () => {
                 placeholder="First Name"
                 style={{
                   height: "40px",
-                  marginRight: "20px",
                   width: "230px",
                   borderRadius: "16px",
                   border: "none",
@@ -142,15 +88,14 @@ const Contact = () => {
                 style={{
                   height: "40px",
                   width: "230px",
-                  marginLeft: "20px",
                   borderRadius: "16px",
                   border: "none",
                 }}
                 value={textareaValue4}
                 onChange={(e) => setTextareaValue4(e.target.value)}
               />
-            </div>
-            <div style={{ marginLeft: "65px", marginTop: "20px" }}>
+            </div> 
+            <div>
               <input
                 className="placeholder"
                 type="email"
@@ -158,7 +103,7 @@ const Contact = () => {
                 placeholder="Email"
                 style={{
                   width: "500px",
-                  height: "40px",
+                  height: "45px",
                   borderRadius: "16px",
                   border: "none",
                 }}
@@ -166,7 +111,7 @@ const Contact = () => {
                 onChange={(e) => setTextareaValue3(e.target.value)}
               />
             </div>
-            <div style={{ marginLeft: "65px", marginTop: "20px" }}>
+            <div >
               <input
                 className="placeholder"
                 type="text"
@@ -187,39 +132,35 @@ const Contact = () => {
               name="message"
               placeholder="Message..."
               style={{
-                marginLeft: "65px",
-                marginTop: "20px",
                 paddingTop:'10px',
                 width: "500px",
                 height: "240px",
                 borderRadius: "16px",
                 border: "none",
-                resize:'none'
+                resize: "none",
               }}
               value={textareaValue}
               onChange={(e) => setTextareaValue(e.target.value)}
             />
-            <input
+            {/* <input
               type="submit"
               value="SEND"
               className="send"
               style={{
                 width: "100px",
                 height: "35px",
-                marginLeft: "465px",
-                marginTop: "20px",
-                fontFamily:'boldFont',
-                border:'none',
-                outline:'none',
-                borderRadius:'20px',
-                fontSize:'19px'
+                
+                fontFamily: "boldFont",
+                border: "none",
+                outline: "none",
+                borderRadius: "20px",
+                fontSize: "19px",
               }}
-            />
-                
+            />  */}
+                 
           </form>
         </div>
       </div>
-      <FooterWithoutClients/>
     </div>
   );
 };
