7151badf4c7d906710577ac8b743907dbc7ec305
Farah Nagy <farahnagy529@gmail>
2023-09-22 04:20:34 +0300

    did the scrollup functionality!!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about-us";
 import Services from "./pages/Services";
 import Platforms from "./pages/Platforms";
 import Contact from "./pages/Contact";
 import Sidebar from "./components/Navbar/SideBar";
+
+
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +31,7 @@ function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <Routes>
